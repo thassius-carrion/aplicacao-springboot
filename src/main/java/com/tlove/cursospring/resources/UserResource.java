960ee41e4cd625392a9325d3a2f3223e49051e50
THASSIUS <thassius.carrion@hotmail.com>
2021-05-12 11:02:53 -0300

    user entity and userResource created

---
+++ src/main/java/com/tlove/cursospring/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.tlove.cursospring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tlove.cursospring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "123456", "pass");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+}
