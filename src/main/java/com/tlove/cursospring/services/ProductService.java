f10e454873f771d30ae3a60b29e1898363a8ae4a
THASSIUS <thassius.carrion@hotmail.com>
2021-05-20 14:51:35 -0300

    entity Product

---
+++ src/main/java/com/tlove/cursospring/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.tlove.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tlove.cursospring.entities.Product;
+import com.tlove.cursospring.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
